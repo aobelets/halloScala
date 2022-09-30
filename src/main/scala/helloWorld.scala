75f3ebfff89cedbf458699c45bdefc03033893fa
aobelets <aobelec3@gmail.com>
2022-09-30 23:04:53 +0200

    second commit

--- src/main/scala/helloWorld.scala
+++ src/main/scala/helloWorld.scala
@@ -1,3 +1,11 @@
 object helloWorld {
+  import java.time.LocalDateTime
+  import scala.io.StdIn
 
+  object Main extends App {
+    //def main(args: Array[String]): Unit = {
+    val d = LocalDateTime.now().getDayOfWeek
+    val name = StdIn.readLine("Enter your name:")
+    println(s"Hello, $name! Today is $d")
+  }
 }
