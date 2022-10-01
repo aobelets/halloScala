2406dd9f57aa58c0f3c335288fc9d829862ac415
aobelets <aobelec3@gmail.com>
2022-10-01 14:20:11 +0200

    current commit

--- src/main/scala/helloWorld.scala
+++ src/main/scala/helloWorld.scala
@@ -1,11 +1,13 @@
-object helloWorld {
+
   import java.time.LocalDateTime
   import scala.io.StdIn
 
-  object Main extends App {
+  object helloWorld extends App {
     //def main(args: Array[String]): Unit = {
     val d = LocalDateTime.now().getDayOfWeek
+    val c = LocalDateTime.now().getYear
     val name = StdIn.readLine("Enter your name:")
     println(s"Hello, $name! Today is $d")
+    println(s"Now is $c year")
   }
-}
+
